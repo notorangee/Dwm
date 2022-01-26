c3a605b30e944818fcba21da63ebad277d4628f8
AvavaAvaOrange <yundaoju@gmail.com>
2022-01-26 12:08:32 +0800

    脚本重构 Dwm Bug修复

--- Script/dwm_script.sh
+++ Script/dwm_script.sh
@@ -7,47 +7,42 @@ while true; do
         #WIFI定义
         WIFI_ICON=''
         NO_WIFI_ICON='睊'
-        WIFI_STATUS='睊'
+	WIFI_STATUS="$NO_WIFI_ICON:0%"
         percentage="0%"
-        WIFI_STRENGTH="0%"
         if [ $( cat /proc/net/wireless | awk 'END{print $0}' | awk -F ':' '{print $1}' ) = "wlan0" ]
         then
                 percentage="$(grep "^\s*w" /proc/net/wireless | awk '{ print "", int($3 * 100 / 70)}'| xargs)"
                 if [ !$percentage ]
                 then
-                        WIFI_STATUS=$WIFI_ICON
-                        WIFI_STRENGTH="$percentage%"
-                else
-                        WIFI_STATUS=$NO_WIFI_ICON
-                        WIFI_STRENGTH="0%"
+			WIFI_STATUS="$WIFI_ICON:$percentage%"
                 fi
-        else
-                WIFI_STATUS=$NO_WIFI_ICON
-                WIFI_STRENGTH="0%"
         fi
-        #蓝牙定义
+
+        #电池定义
+        BAT_COUNT=$(acpi -b | awk '{ print $4 }' | awk -F '%' '{print $1}')
+        case $BAT_COUNT in
+                  *|10|[0-9])  BAT_ICON="ﴏ" ;;
+                2[0-5]|1[1-9]) BAT_ICON="" ;;
+                3[0-9]|2[6-9]) BAT_ICON="" ;;
+                5[0-5]|4[0-9]) BAT_ICON="" ;;
+                6[0-9]|5[6-9]) BAT_ICON="" ;;
+                8[0-5]|7[0-9]) BAT_ICON="" ;;
+                9[0-5]|8[6-9]) BAT_ICON="" ;;
+                   100|9[6-9]) BAT_ICON="" ;;
+        esac
+	BAT_STATUS="$BAT_ICON:$BAT_COUNT%"
+
+	#蓝牙定义
         BLUE_DEVICE=$( bluetoothctl info | awk '/Alias/ {print $2}' )
         BLUE_STATUS=""
 
         if [ $BLUE_DEVICE ]
         then
-                BLUE_STATUS=""
+                BLUE_STATUS=":$BLUE_DEVICE"
         else
-                BLUE_DEVICE="NO_DEVICE"
+        	BLUE_STATUS=":NDC"
         fi
-        #电池定义
-        BAT_COUNT=$(acpi -b | awk '{ print $4 }' | awk -F '%' '{print $1}')
-        BAT="$BAT_COUNT%"
-        case $BAT_COUNT in
-                    10|[0-9])  BAT_STATUS="ﴏ" ;;
-                2[0-5]|1[1-9]) BAT_STATUS="" ;;
-                3[0-9]|2[6-9]) BAT_STATUS="" ;;
-                5[0-5]|4[0-9]) BAT_STATUS="" ;;
-                6[0-9]|5[6-9]) BAT_STATUS="" ;;
-                8[0-5]|7[0-9]) BAT_STATUS="" ;;
-                9[0-5]|8[6-9]) BAT_STATUS="" ;;
-                   100|9[6-9]) BAT_STATUS="" ;;
-        esac
+
         #状态栏定义
         if [ $BAT_COUNT -lt 10 ]
         then
@@ -56,7 +51,10 @@ while true; do
         fi
         LOCALTIME=$( date +'%F %A %T' )
         BACKLIGHT_INFO=`echo "scale=2; ($( cat /sys/class/backlight/amdgpu_bl0/actual_brightness ) / 255) * 100" | bc`
-        BACKLIGHT="${BACKLIGHT_INFO%.*}%"
+        BACKLIGHT="ﱧ:${BACKLIGHT_INFO%.*}%"
         VOL=$( amixer get Master | awk -F'[][]' 'END{ print $2 }' )
-        xsetroot -name " $BLUE_STATUS:$BLUE_DEVICE $WIFI_STATUS:$WIFI_STRENGTH ﱧ:$BACKLIGHT ﰝ:$VOL $BAT_STATUS:$BAT $LOCALTIME "
+	VOL_STATUS="ﰝ:$VOL"
+        xsetroot -name " $BLUE_STATUS $WIFI_STATUS $BACKLIGHT $VOL_STATUS $BAT_STATUS $LOCALTIME "
+
 done &
+
