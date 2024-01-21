1eb8c1390541adc4378c17fa69fcb69df243c5fb
AvavaAvaOrange <yundaoju@gmail.com>
2024-01-21 18:29:10 +0800

    添加鼠标滚轮调节亮度功能

--- Scripts/system/dwm_backlight.sh
+++ Scripts/system/dwm_backlight.sh
@@ -3,6 +3,8 @@
 case $BLOCK_BUTTON in
   1) light -A 5 || notify-send "$( printf '%s\n' '增大屏幕背光错误，请查看日志')" ;;
   3) light -U 5 || notify-send "$( printf '%s\n' '减小屏幕背光错误，请查看日志')" ;;
+  4) light -A 5 ;;
+  5) light -U 5 ;;
 	6) alacritty -e $EDITOR "$0" ;;
 esac
 
