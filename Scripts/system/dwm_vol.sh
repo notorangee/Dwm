acfe3cd2e9f0f34fa55e9197636211f000491522
AvavaAvaOrange <yundaoju@gmail.com>
2024-01-31 19:27:25 +0800

    调整配置，优化音量脚本

--- Scripts/system/dwm_vol.sh
+++ Scripts/system/dwm_vol.sh
@@ -1,12 +1,9 @@
 #! /bin/sh
 
 case $BLOCK_BUTTON in
-  1) (mpc pause-if-playing >/dev/null;
-    if [[ $? -ne 0 ]]; then
-      mpc play >/dev/null && notify-send "󰝚播放音乐" "$(printf '%s %s %s\n' $(mpc current))"
-    else
-      mpc pause >/dev/null && notify-send "󰝚暂停音乐" "$(printf '%s %s %s\n' $(mpc current))" 
-      fi) ;;
+  1) mpc pause-if-playing >/dev/null \
+        && notify-send "󰝚暂停音乐" "$(printf '%s %s %s\n' $(mpc current))" \
+        || (mpc play >/dev/null && notify-send "󰝚播放音乐" "$(printf '%s %s %s\n' $(mpc current))");;
 	2) notify-send "󰝚音乐信息" "$(printf '%s %s %s\n' $(mpc current))" ;;
 	3) mpc next >/dev/null && notify-send "󰝚下一首音乐" "$(printf '%s %s %s\n' $(mpc current))" ;;
   4) wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+;;
