21c3bb9d969f8f138f0c352c9772b8ddde419214
AvavaAvaOrange <yundaoju@gmail.com>
2024-02-05 17:16:59 +0800

    优化音乐播放时的通知超时

--- Scripts/system/dwm_vol.sh
+++ Scripts/system/dwm_vol.sh
@@ -2,10 +2,10 @@
 
 case $BLOCK_BUTTON in
   1) mpc pause-if-playing >/dev/null \
-        && notify-send "󰝚暂停音乐" "$(printf '%s %s %s\n' $(mpc current))" \
-        || (mpc play >/dev/null && notify-send "󰝚播放音乐" "$(printf '%s %s %s\n' $(mpc current))");;
-	2) notify-send "󰝚音乐信息" "$(printf '%s %s %s\n' $(mpc current))" ;;
-	3) mpc next >/dev/null && notify-send "󰝚下一首音乐" "$(printf '%s %s %s\n' $(mpc current))" ;;
+        && notify-send -t 3000 "󰝚暂停音乐" "$(printf '%s %s %s\n' $(mpc current))" \
+        || (mpc play >/dev/null && notify-send -t 3000 "󰝚播放音乐" "$(printf '%s %s %s\n' $(mpc current))");;
+	2) notify-send -t 5000 "󰝚音乐信息" "$(printf '%s %s %s\n' $(mpc current))" ;;
+	3) mpc next >/dev/null && notify-send -t 1500 "󰝚下一首音乐" "$(printf '%s %s %s\n' $(mpc current))" ;;
   4) wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+;;
   5) wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%-;;
 	6) alacritty -e $EDITOR "$0" ;;
