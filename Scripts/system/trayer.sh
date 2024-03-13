13ecefb2a97ba2d373c069403e38b2f5cf8f3a36
AvavaAvaOrange <yundaoju@gmail.com>
2024-03-14 00:24:42 +0800

    调整trayer配置

--- Scripts/system/trayer.sh
+++ Scripts/system/trayer.sh
@@ -2,7 +2,7 @@
 
 killall trayer 2>/dev/null
 if [ $? -ne 0 ]; then
-  	trayer --transparent true --expand false --edge top --align center --width 5 --height 20 --distance 2 \
-      --distancefrom top --SetDockType false --tint 0x00000000 --iconspacing 10 &
+  	trayer --transparent true --expand true --edge top --align center --margin 5 --padding 5 --width 5 --height 20 --distance 3 \
+      --distancefrom top --SetDockType false --tint 0x00000000 --iconspacing 2 &
 fi
 
