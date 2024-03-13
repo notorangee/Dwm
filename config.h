c2503abc162ceb5ddc5dcf378af6cf5ccf7aed5a
AvavaAvaOrange <yundaoju@gmail.com>
2024-03-13 23:52:57 +0800

    去掉dwm-systray托盘补丁，存在性能问题。重新加入alpha补丁替代picom合成器实现透明bar效果

--- config.h
+++ config.h
@@ -5,13 +5,6 @@ static const unsigned int dwmmode  = 0;        /* laptop = 0 or tablet = 1 */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 3;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 1;    /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
-
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 3;       /* vertical padding of bar */
@@ -29,10 +22,21 @@ static const char col_cyan[]        = "#005577";//蓝色
 // dwm6.3 update
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+//alpha补丁
+static const unsigned int baralpha = 0xd0;
+static const unsigned int borderalpha = OPAQUE;
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray3, col_gray4, col_gray4  },
+};
+
+//alpha补丁
+static const unsigned int alphas[][3]      = {
+       /*               fg      bg        border     */
+       [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+       [SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
@@ -100,7 +104,7 @@ static const char *scratchpadcmd[] = { "alacritty", "-t" ,scratchpadname, NULL }
 /*其它小窗口*/
 static const char *bluetuithcmd[] = {"alacritty", "-t", "bluetuith", "-e", "bluetuith", NULL };
 static const char *musiccmd[] = {"alacritty", "-t", "ncmpcpp", "-e", "ncmpcpp", NULL };
-static const char *musicfoxcmd[] = {"alacritty", "-t", "musicfox", "-e", "musicfox", NULL };
+static const char *systraycmd[] = {"/home/orange/Dwm/Scripts/system/trayer.sh", NULL };
 /*锁屏*/
 static const char *slockcmd[] = { "/home/orange/Dwm/Scripts/system/i3lock.sh", NULL };
 static const char *forceoffandclockcmd[] = { "/home/orange/Dwm/Scripts/system/forceoff_lock.sh", NULL };
@@ -126,7 +130,7 @@ static const Key keys[] = {
   { MODKEY,                       XK_F5,     spawn,          SHCMD("light -U 5; pkill -RTMIN+4 dwmblocks") },
   { MODKEY,                       XK_F6,     spawn,          SHCMD("light -A 5; pkill -RTMIN+4 dwmblocks") },
   { MODKEY,                       XK_F7,     spawn,          {.v = slockcmd } } ,
-  { MODKEY,                       XK_F8,     spawn,          {.v = musicfoxcmd } } ,
+  { MODKEY,                       XK_F8,     spawn,          {.v = systraycmd } } ,
   { MODKEY,                       XK_F9,     spawn,          {.v = musiccmd } } ,
   { MODKEY,                       XK_F10,    spawn,          {.v = touchpadcmd } } ,
   { MODKEY,                       XK_F11,    spawn,          {.v = forceoffandclockcmd } } ,
@@ -206,11 +210,12 @@ static const Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button2,        spawn,          {.v = forceoffandclockcmd } },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+
+  { ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 
 	{ ClkWinTitle,          0,              Button1,        spawn,          {.v = dmenucmd} }, // 左键点击中间弹出rofi应用
 	{ ClkWinTitle,          0,              Button3,        killclient,     {0} }, // 中键点击中间退出应用
