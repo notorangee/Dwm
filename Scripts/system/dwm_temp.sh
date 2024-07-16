955592819790014034644b9c3cae36809cfa2d06
AvavaAvaOrange <yundaoju@gmail.com>
2024-07-16 17:26:16 +0800

    添加CPU温度显示

--- Scripts/system/dwm_temp.sh
+++ Scripts/system/dwm_temp.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-temperature=$(echo "$(cat /sys/class/hwmon/hwmon2/temp1_input) / 1000" | bc)
+temperatureC=$(echo "$(cat /sys/class/hwmon/hwmon4/temp1_input) / 1000" | bc)
+temperatureD=$(echo "$(cat /sys/class/hwmon/hwmon2/temp1_input) / 1000" | bc)
 
 case $BLOCK_BUTTON in
-  1) notify-send "$(printf '%s\n' '温度信息')" "$(printf '%s%s\n' \
-    '机身温度:' $temperature)""$(printf '\n%s%s\n' 'CPU温度:' $(printf '%.0f%s' \
-    60))";;
+  1) notify-send "$(printf '%s\n' '温度信息')" "$(printf '%s%d%s\n' \
+    '机身温度:' $temperatureD '°C')""$(printf '\n%s%d%s\n' 'CPU温度:' $temperatureC '°C')";;
 	2) notify-send "temp2" ;;
 	6) alacritty -e $EDITOR "$0" ;;
 esac
 
-printf ":%d°C\n" "${temperature}"
+printf ":%d°C\n" "${temperatureD}"
