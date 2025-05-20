8512cb57f90b544ebd0606346f095606e44887ea
AvavaAvaOrange <yundaoju@gmail.com>
2025-05-20 13:50:56 +0800

    Temp Script:Modify Temperature Description

--- Scripts/system/dwm_temp.sh
+++ Scripts/system/dwm_temp.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+temperatureB=$(echo "$(cat /sys/class/hwmon/hwmon1/in0_input) / 1000" | bc)
 temperatureC=$(echo "$(cat /sys/class/hwmon/hwmon4/temp1_input) / 1000" | bc)
-temperatureD=$(echo "$(cat /sys/class/hwmon/hwmon2/temp1_input) / 1000" | bc)
 
 case $BLOCK_BUTTON in
   1) notify-send "$(printf '%s\n' '温度信息')" "$(printf '%s%d%s\n' \
-    '机身温度:' $temperatureD '°C')""$(printf '\n%s%d%s\n' 'CPU温度:' $temperatureC '°C')";;
+    '电池温度:' $temperatureB '°C')""$(printf '\n%s%d%s\n' 'CPU温度:' $temperatureC '°C')";;
 	2) notify-send "temp2" ;;
 	6) alacritty -e $EDITOR "$0" ;;
 esac
 
-printf ":%d°C\n" "${temperatureD}"
+printf ":%d°C\n" "${temperatureC}"
