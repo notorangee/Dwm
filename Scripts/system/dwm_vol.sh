c91f7de7673c3761a79a3efbb93bdc0b9c49b60f
AvavaAvaOrange <yundaoju@gmail.com>
2025-06-04 15:35:13 +0800

    Vol Script:Do not issue notifications when ncmpcpp is running to avoid conflicts with notifications in ncmpcpp

--- Scripts/system/dwm_vol.sh
+++ Scripts/system/dwm_vol.sh
@@ -1,11 +1,16 @@
 #! /bin/sh
 
+ps -A | grep ncmpcpp >/dev/null
+if [[ $? -eq 0 ]]; then
+  PLAY_STATUS=true
+fi
+
 case $BLOCK_BUTTON in
   1) mpc pause-if-playing >/dev/null \
         && notify-send -t 3000 "󰝚暂停音乐" "$(printf '%s %s %s\n' $(mpc current))" \
         || (mpc play >/dev/null && notify-send -t 3000 "󰝚播放音乐" "$(printf '%s %s %s\n' $(mpc current))");;
 	2) notify-send -t 5000 "󰝚音乐信息" "$(printf '%s %s %s\n' $(mpc current))" ;;
-	3) mpc next >/dev/null && notify-send -t 1500 "󰝚下一首音乐" "$(printf '%s %s %s\n' $(mpc current))" ;;
+	3) mpc next >/dev/null && [[ $PLAY_STATUS ]] || notify-send -t 1500 "󰝚下一首音乐" "$(printf '%s %s %s\n' $(mpc current))" ;;
   4) wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+;;
   5) wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%-;;
 	6) alacritty -e $EDITOR "$0" ;;
