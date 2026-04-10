f2c172bf1e1da62cbf93cc1f868e8b55c098f102
notorangee <yundaoju@gmail.com>
2026-04-11 01:43:02 +0800

    Fixed Cava Not Being Closed When Music is Pause

--- Scripts/system/dwm_music.sh
+++ Scripts/system/dwm_music.sh
@@ -1,9 +1,10 @@
 #! /bin/zsh
 
 name_pid=$(pgrep -fn dwm_musicinfo.sh)
+cava_pid=$(pgrep -fn cava)
 [[ ? -eq 0 ]] && kill -9 ${name_pid} 2>/dev/null
 killall ncmpcpp 2>/dev/null
-killall cava 2>/dev/null
+kill -9 ${cava_pid} 2>/dev/null
 if [ $? -ne 0 ]; then
   alacritty -t MusicWin -e ncmpcpp & \
   alacritty -t MusicVisua -e cava -p $HOME/.config/cava/dwm & \
